f4b3cc3a6ff455b3c54166768438e9b7ebae4c7a
reivertt <billthehuman27@gmail.com>
2025-06-12 23:19:06 +0700

    fix: configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "ikan.bakar.ini_berapa"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
